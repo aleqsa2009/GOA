3367710305def6adf7a36ef1eae64c3a389bfc4a
aleqsa2009 <kotorashvilialeqsandre8@gmail.com>
2024-02-13 16:10:40 +0400

    Day three

---
+++ Day 04/classwork/Day 04.py
@@ -0,0 +1,10 @@
+my_name = "aleqsandre"
+#my_name არის ცვლადის სახელი
+# = არის მიმნიჭებელი ოპერატორი 
+#aleqsandre არის ცვლადის მნიშვნელობა (variable value)
+
+print(my_name)
+x=5
+y=10
+
+print(x+y)
