fec8a2eefbe658b09f94c291d3d5d655302eeeb9
aleqsa2009 <kotorashvilialeqsandre8@gmail.com>
2024-02-24 21:51:19 +0400

    Day 07

---
+++ Day 06/homework/lesson 06.py
@@ -0,0 +1,8 @@
+num0=int(input("enter your number"))
+num1=int(input("enter your number"))
+num2=int(input("enter your number"))
+
+print(num0 + num1 + num2)
+print(num0 - num1 - num2)
+print(num0 * num1 * num2)
+print(num0 / num1 / num2)
