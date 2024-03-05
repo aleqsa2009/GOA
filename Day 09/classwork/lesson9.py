2608c27f91cae25c36d4602f36f48b3917a26f2b
aleqsa2009 <kotorashvilialeqsandre8@gmail.com>
2024-03-05 21:37:56 +0400

    day 09

---
+++ Day 09/classwork/lesson9.py
@@ -0,0 +1,24 @@
+print(range(7,22))
+
+
+print(range(0,11))
+
+
+print(range(10,20))
+
+
+
+
+for num0 in range(1,11):
+    print(num0)
+
+
+
+
+for num1 in range(1,20):
+    print(num1)
+    
+
+
+for name in range(10):
+    print("aleqsandre")
---
+++ Day 09/homework/lesson9.py
@@ -0,0 +1,29 @@
+#1
+for i in range(1,21):
+    print(i)
+
+#2
+i=(int(input("number1:")))
+ii=(int(input("number2:")))
+for i in range(1,20):
+    print(i)
+#3
+for i in range(50,101):
+    print(i)
+#4
+for i in range(100,49,-1):
+    print(i)
+#5
+for i in range(51):
+    print(i)
+
+#6
+i1 = (int(input("enter your number:"))) #74
+for i in range(74):
+    print(i)
+#7
+age_now = (int(input("enter your age:"))) #14
+age_from_now = (int(input("enter your age with 10 years in the future:"))) #24
+for i in range(14,24):
+    print(i)
+    
