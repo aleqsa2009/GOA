a04647336e3ee0dfc704d3b35180e488bdc95134
aleqsa2009 <kotorashvilialeqsandre8@gmail.com>
2024-03-19 22:39:53 +0400

    Day 14

---
+++ Day 13/homework/lesso13.py
@@ -0,0 +1,62 @@
+for i in range(1,50,3):
+    print(i)
+    print("fizz")
+for i in range(1,50,5):
+    print(i)
+    print("buzz")
+
+for i in range(1,50,15):
+    print(i)
+    print("fizzbuzz")
+
+
+i=int(input("enter your number :"))
+if i > 0 : 
+    print("your number is positive")
+
+elif i <0 :
+    print("your number is negative")
+
+else :
+    print("your number = 0")
+    
+
+for i in range(1,100):
+    print(i)
+
+
+i = int(input("enter number between 1-7 :")) 
+
+
+if i == 1:
+    print("შაბათი")
+
+elif i == 2:
+    print("კვირა")
+
+elif i == 3:
+    print("ორშაბათი")
+
+elif i == 4:
+    print("სამშაბათი")
+
+elif i == 5 :
+    print("ოთხშაბათი")
+
+elif i == 6 :
+    print("ხუთშაბათი")
+
+elif i == 7 :
+    print("პარასკევი")
+    
+
+i = int(input("enter your number : "))
+
+if i  in range(1,99999999999999,2):
+    print("კენტი")
+
+elif i in range(1,99999999999999,3):
+    print("ლუწი")
+else :
+    print("რიცხვი = 0")
+
